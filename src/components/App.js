daeafa377da745f988325fb701ce75c22034aefe
s2j55 <s2jeongyang@gmail.com>
2021-02-26 23:17:09 +0900

    2.0 auth

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import react, { useState } from 'react';
+import AppRouter from "components/Router";
+import { authService } from "fbase";
+
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+  <>
+  <AppRouter isLoggedIn={isLoggedIn} />
+  <footer>&copy; Switter {new Date().getFullYear()} </footer>
+  </>
+  )
+}
+
+export default App;
