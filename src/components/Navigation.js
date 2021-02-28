e1c97be177c2f7b66b6053a470a79ccce600ec51
s2j55 <s2jeongyang@gmail.com>
2021-02-28 22:00:08 +0900

    5.1 Update Profile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-const Navigation = () => {
+const Navigation = ({userObj}) => {
 return (
     <>
     <nav>
         <ul>
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/profile">{userObj.displayName}'s Profile</Link></li>
         </ul>
     </nav>
     </>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,26 +1,54 @@
-import { authService, dbService } from 'fbase';
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import { authService } from 'fbase';
 // import { useHistory } from "react-router-dom";
 
 const Profile = ({userObj}) => {
     // const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const onLogOutClick = () => 
         authService.signOut();
        // history.push('/');                                                   
     ;
-    const getMySweets = async() => {
+    const onChange = event => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
+    };
+    /* const getMySweets = async() => {
         const sweets = await dbService
             .collection("sweets")
             .where("creatorId", "==", userObj.uid)
             .orderBy("createdAt", "desc")
             .get();
         console.log(sweets.docs.map(doc => doc.data()));
-    };
-    useEffect(() => {
+    }; */
+    /* useEffect(() => {
         getMySweets();
     }, []);
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }; */
+// Edit my name(my profile)
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                // TODO photoURL도 만들어서 넣어보기
+                displayName: newDisplayName,
+            });
+        }
+    };
     return (
     <>
+    <form onSubmit={onSubmit}>
+        <input 
+        onChange={onChange}
+        type="text" 
+        placeholder="Display name"
+        value={newDisplayName} />
+        <input type="submit" value="Update Profile" />
+    </form>
         <button onClick={onLogOutClick}>Log Out</button>
     </>
 )};
