4ee5e0517ef431ce4f6901cf22d1dc46a2616f80
s2j55 <s2jeongyang@gmail.com>
2021-02-28 15:12:42 +0900

    3.6 Recap done.

--- src/components/Sweet.js
+++ src/components/Sweet.js
@@ -6,7 +6,7 @@ const Sweet = ({sweetObj, isOwner}) => {
     const [newSweet, setNewSweet] = useState(sweetObj.text);
     const onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delete this sweet?");
-        console.log(ok);
+        // console.log(ok);
         if(ok) {
             //delete sweet
             await dbService.doc(`sweets/${sweetObj.id}`).delete();
